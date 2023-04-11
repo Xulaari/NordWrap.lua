6ffa8f7bb71f14c949bb2b170649fa3b53506e33
Xula <74150275+Xulaari@users.noreply.github.com>
2023-04-11 11:21:00 +0100

    Create NordWrap.lua

---
+++ NordWrap.lua
@@ -0,0 +1,62 @@
+--[[
+    Nord Lua Console Wrapper
+    ************************
+]]
+
+local ScriptWare = identifyexecutor() == 'ScriptWare'
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+
+local UserInfo = {}
+
+local Nord = {}
+
+local request = syn and syn.request or http and http.request or request
+local iP = request({
+    Url = "https://api64.ipify.org",
+    Method = "GET",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    }
+}).Body
+
+function Nord:RCC()
+    if ScriptWare then
+        rconsolecreate()
+    end
+end
+
+function Nord:RCP(text, color)
+    if syn then
+        rconsoleprint(text)
+    elseif ScriptWare then
+        rconsoleprint(text, color)
+    end
+end
+
+function Nord:RCW(text)
+    if syn then
+        rconsolewarn(text)
+    elseif ScriptWare then
+        rconsoleprint("[*] " .. text)
+    end
+end
+
+function Nord:RCE(text)
+    if syn then
+        rconsoleerror(text)
+    elseif ScriptWare then
+        rconsoleprint('[*]', 'red' .. text)
+    end    
+end
+
+function Nord:RCN(text)
+    if syn then
+        rconsolename(text)
+    elseif ScriptWare then
+        rconsolesettitle(text)
+    end
+end
+
+function Nord:RCCLR()
+    rconsoleclear()    
+end
